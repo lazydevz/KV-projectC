7d4b5bfacaab30a0b152c783a476576398cf1c2c
Mindaugas <gailiusas.m@gmail.com>
2026-06-29 12:10:00 -0400

    add kv_get

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <kv.h>
+#include <string.h>
 
 
 int main() {
@@ -8,10 +9,12 @@ int main() {
     printf("%ld\n", table->capacity);
 
     kv_put(table, "hehe", "haha");
-    kv_put(table, "hehe", "hoho");
     kv_put(table, "lala", "hoho");
+    kv_put(table, "john", "kek");
+    kv_put(table, "test1", "test");
+    kv_put(table, "name", "alice");
 
-
+    
     for (int i = 0; i < table->capacity; i++) {
         if (table->entries[i].key) {
             printf("[%d] %s: %s\n",
@@ -20,4 +23,13 @@ int main() {
                 table->entries[i].value);
         }
     }
+
+    char *val = kv_get(table, "heheg");
+
+    if (val)
+    printf("%s\n", val);
+    else
+    printf("not found\n");
+
+    kv_free(table);
 }
